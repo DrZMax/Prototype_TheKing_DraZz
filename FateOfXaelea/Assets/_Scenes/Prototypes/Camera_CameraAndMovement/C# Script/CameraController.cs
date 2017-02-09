b55e5701b7e7f61d826659dd0e0b10209e3b6dd3
Maxime Saindon Turcotte <drazz951@gmail.com>
2017-02-08 20:35:39 -0500

    [DEV] - Camera/Movement/Animation
    Déplacement du personnage principal
    Animation du personnage principal

---
+++ FateOfXaelea/Assets/_Scenes/Prototypes/Camera_CameraAndMovement/C# Script/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject FollowTarget;             //Target que la caméra va suivre
+    public float CameraMoveSpeed;               //Vitesse de la caméra qui suit la target
+    private Vector3 _targetPosition;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+        _targetPosition = new Vector3(FollowTarget.transform.position.x, FollowTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, _targetPosition, CameraMoveSpeed * Time.fixedDeltaTime);
+
+    }
+}
